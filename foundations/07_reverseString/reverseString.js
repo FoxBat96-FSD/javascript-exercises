8649483868892424748afb01bc54106f4d4e42ee
Wilhelm Blaauw <272574467+FoxBat96-FSD@users.noreply.github.com>
2026-09-11 13:56:40 +0200

    feat: implement reverseString solution
    - Add reverseString function using .split(''), .reverse(), and .join('')
    - Pass all unit tests for string reversal

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+// PSEUDOCODE:
+// 1. Convert string into an array of single characters using .split('')
+// 2. Reverse the array using .reverse()
+// 3. Glue characters back together into a string using .join('')
 
+const reverseString = function(string) {
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
